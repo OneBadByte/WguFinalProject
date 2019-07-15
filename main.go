870cdff7dc85705d10202df8519c9639c820f3e5
brody prestwich <brody.prestwich18@gmail.com>
2019-07-14 22:12:02 -0600

    added website files and the go server will also serve those files

--- main.go
+++ main.go
@@ -3,16 +3,26 @@ package main
 import (
 	_ "fmt"
 	"github.com/gin-gonic/gin"
-	_ "net/http"
+	"net/http"
+	"os/exec"
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/ping", func(context *gin.Context) {
-		context.JSON(200, gin.H{
-			"message": "pong",
-		})
+	// updates everything from github
+	pullFromGithub := exec.Command("git", "pull")
+	pullFromGithub.Run()
 
+	router := gin.Default()
+
+	// serves all the html, css, and javascript files to the browser
+	router.StaticFile("/", "./web/html/index.html")
+	router.StaticFS("/html", http.Dir("./web/html/"))
+	router.StaticFS("/css", http.Dir("./web/css/"))
+	router.StaticFS("/js", http.Dir("./web/js/"))
+}
+
+func ping(context *gin.Context) {
+	context.JSON(200, gin.H{
+		"message": "pong",
 	})
-	router.Run()
 }
